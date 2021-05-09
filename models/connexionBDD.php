f757a2239d6ecf281a1093a0683272eed4b72960
Jean-Laily <jean.yansin@gmail.com>
2021-05-09 20:30:03 +0400

    maj_1.9.2

--- models/connexionBDD.php
+++ models/connexionBDD.php
@@ -8,28 +8,19 @@
      * @param null
      */
     function getConnect(){
-        //initialisation des variables et affectations des valeurs par défaut 
-        $url = "localhost";             //@ip.... par ex : 127.0.0.1
-        $port = '';                     //port ip ... par ex : "3306" par défaut
-        $char = 'UTF8';                 //charset ... défini le type d'encodage souhaité
-        $bdd = 'appsgao';                // nom de la data base
-        $login = "root";                
-        $pass = "";
-
-        //try catch va permettre de capturer l'erreur et de retourner un null s'il y a exception
-        try{
-            //création de la connection vers un SGBD avec l'extension PHP Data Object 
-            $serveur = 'mysql:host=' .  $url  .';dbname='. $bdd;
-            //on remet ici le port et le charset avec les paramètre par défaut afin d'éviter tout problème de connexion
-            if($port != '') $serveur .= ';port='. $port; 
-            if($char != '') $serveur .= ';charset='. $char;
-
-            //instantiation de l'objet PDO et de ses paramètres requis
-            $pdo = new PDO($serveur, $login, $pass); 
-            // $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-        }catch(PDOException $e){
-            // echo 'Échec de la connection : ';
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $database = "appsgao";
+        $char = 'UTF8';
+        
+        try {
+            $pdo = new PDO("mysql:host=$servername;dbname=$database;charset=$char", $username, $password );
+            // set the PDO error mode to exception
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            echo "Connected successfully"; 
+        } catch(PDOException $e) {  
+            echo "Connection failed: " . $e->getMessage();
             $pdo = null;
         }
 
